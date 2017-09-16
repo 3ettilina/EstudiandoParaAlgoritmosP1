fcd295c932fbdf1ce8a940506fc4eb2c060e6d73
Bettilina <betticarrizo@gmail.com>
2017-09-16 16:15:18 -0300

    UT3 Rep 5 - Ejercicio 1

---
+++ EstudiandoParaAlgoritmos/src/UT3/Rep5Ej1/Main.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/*
+* UNIDAD TEMÁTICA 3 – DICCIONARIOS, MAPAS & HASHING
+* PRACTICOS DOMICILIARIOS INDIVIDUALES - 5
+* EJERCICIO 1
+* 
+* Ahora que conocemos la librería de colecciones de JAVA, deseamos mejorar nuestra implmentación
+* del TRIE que realizamos en la Unidad Temática 2, para hacerla más flexible y fácilmente adaptable a
+* diferentes alfabetos y contextos.
+* 
+* Se propone entonces utilizar, como forma de almacenamiento de los enlaces a los subárboles en los
+* nodos del TRIE, una estructura de HASHMAP.
+* 
+* Utilizando entonces los ejemplos y código de la UT2, desarrolla:
+*
+* • Nuevas clases TTrieHashMap y TNodoTrieHashMap (ésta última utiliza para la
+* representación un hashMap, en vez del vector fijo )
+* • Adapta los métodos de inserción, búsqueda, búsqueda de prefijos (“predecir”) y búsqueda
+* de patrones (vistos en la aplicación de tries como árboles de sufijos)
+* • Implementa nuevamente la aplicación de “autocompletar” , utilizando estas nuevas clases, y
+* haz las pruebas que estimes convenientes.
+* • Implementa también la aplicación para la búsqueda de patrones en un cierto texto,
+* indicando las posiciones del texto en que ocurren
+*
+*/
+
+package UT3.Rep5Ej1;
+
+/**
+ *
+ * @author Bettina
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ EstudiandoParaAlgoritmos/src/UT3/Rep5Ej1/TNodoTrieHashMap.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package UT3.Rep5Ej1;
+
+/**
+ *
+ * @author Bettina
+ */
+public class TNodoTrieHashMap {
+    
+}
