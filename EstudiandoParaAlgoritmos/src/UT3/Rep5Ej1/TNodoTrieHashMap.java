daf8552d452cfbc503149b30f42b5a42bc6c2409
Bettilina <betticarrizo@gmail.com>
2017-09-16 17:19:53 -0300

    Todo limpio

--- EstudiandoParaAlgoritmos/src/UT3/Rep5Ej1/TNodoTrieHashMap.java
+++ EstudiandoParaAlgoritmos/src/UT3/Rep5Ej1/TNodoTrieHashMap.java
@@ -16,72 +16,50 @@ public class TNodoTrieHashMap {
     private HashPiola[] hijos;
 
     public TNodoTrieHashMap() {
-        hijos = new HashPiola(23, 0.5);
-        esPalabra = false;
+        
+        
     }
 
     public void insertar(String unaPalabra) {
-        TNodoTrieHashMap nodo = this;
-        for (int c = 0; c < unaPalabra.length(); c++) {
-            int indice = unaPalabra.charAt(c) - 'a';
-            if (nodo.hijos[indice] == null) {
-                nodo.hijos[indice] = new TNodoTrieHashMap();
-            }
-            nodo = nodo.hijos[indice];
-        }
-        nodo.esPalabra = true;
+        
+    }
+    
+    public int buscar(String palabra, int[] contador){
     }
 
     private void imprimir(String s, TNodoTrieHashMap nodo) {
-        if (nodo != null) {
-            if (nodo.esPalabra) {
-                System.out.println(s);
-
-            }
-            for (int c = 0; c < CANT_CHR_ABECEDARIO; c++) {
-                if (nodo.hijos[c] != null) {
-                    imprimir(s + (char) (c + 'a'), nodo.hijos[c]);
-                }
-            }
-        }
+        
     }
 
     public void imprimir() {
-
-        imprimir("", this);
     }
 
     private TNodoTrieHashMap buscarNodoTrie(String s) {
-        TNodoTrieHashMap nodo = this;
-        for (int c = 0; c < s.length(); c++) {
-            int indice = s.charAt(c) - 'a';
-            if (nodo.hijos[indice] == null) {
-                return null;
-            }
-            nodo = nodo.hijos[indice];
-        }
-        return nodo;
+        
     }
 
     private void predecir(String s, String prefijo, LinkedList<String> palabras, TNodoTrieHashMap nodo) {
-        // implementar
-        if (nodo != null) {
-            if (nodo.esPalabra) {
-                palabras.add(prefijo + s);
-            }
-            for (int c = 0; c < CANT_CHR_ABECEDARIO; c++) {
-                if (nodo.hijos[c] != null) {
-                    predecir(s + (char) (c + 'a'), prefijo, palabras, nodo.hijos[c]);
-                }
-            }
-        }
-
+        
     }
 
     public void predecir(String prefijo, LinkedList<String> palabras) {
-        TNodoTrieHashMap nodo = buscarNodoTrie(prefijo);
-        if (nodo != null) {
-            predecir("", prefijo, palabras, nodo);
-        }
+        
     }
+//<editor-fold defaultstate="collapsed" desc="Getters & Setters">
+    
+
+    /**
+     * @return the hijos
+     */
+    public HashPiola[] getHijos() {
+        return hijos;
+    }
+
+    /**
+     * @param hijos the hijos to set
+     */
+    public void setHijos(HashPiola[] hijos) {
+        this.hijos = hijos;
+    }
+//</editor-fold>
 }
