0be91aee6fc656bc5f5d0e93d3277d30192950ab
Juan Carlos Díaz <jcdpess@gmail.com>
2017-09-18 16:10:32 -0300

    Nueva versión con la búsqueda booleana.

---
+++ EstudiandoParaAlgoritmos/src/UT3/Rep5Ej1/MainArbolesB.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package UT3.Rep5Ej1;
+
+/**
+ *
+ * @author Jotacé
+ */
+public class MainArbolesB {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        for (int i = 0; i < 13; i++) {
+            System.out.println((int) (Math.random() * 100) + 1);
+        }
+    }
+
+}
