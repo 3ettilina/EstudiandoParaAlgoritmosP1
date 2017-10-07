2c08582fc3cc2470ece73b84cdb4ed374a6ff26e
Juanca <Juanca@Jazz-PC>
2017-10-07 00:17:44 -0300

    buscar

---
+++ EstudiandoParaAlgoritmos/src/grafosConEnzo/PruebaGrafo.java
@@ -0,0 +1,42 @@
+package grafosConEnzo;
+
+public class PruebaGrafo {
+
+    public static void main(String[] args) {
+
+//        TGrafoDirigido gd = (TGrafoDirigido) UtilGrafos.cargarGrafo("src/UT04/TA03/ejemploClaseAeropuertos.txt", "src/UT04/TA03/ejemploClaseConexiones.txt",
+//                false, TGrafoDirigido.class);
+        TGrafoDirigido gd = (TGrafoDirigido) UtilGrafos.cargarGrafo("src/grafosConEnzo/aeropuertos.txt", "src/grafosConEnzo/conexiones.txt",
+                false, TGrafoDirigido.class);
+
+        Object[] etiquetasarray = gd.getEtiquetasOrdenado();
+
+        Double[][] matriz = UtilGrafos.obtenerMatrizCostos(gd.getVertices());
+        UtilGrafos.imprimirMatrizMejorado(matriz, gd.getVertices(), "Matriz");
+        Double[][] mfloyd = gd.floyd();
+        UtilGrafos.imprimirMatrizMejorado(mfloyd, gd.getVertices(), "Matriz luego de FLOYD");
+        System.out.println(gd.centroDelGrafo());
+        UtilGrafos.imprimirMatrizMejorado(gd.getMatrizPredecesoresGrafo(), gd.getVertices(), "Matriz Predecesores");
+        gd.warshall();
+        UtilGrafos.imprimirMatrizMejoradoSoportaBoolean(gd.getMatrizWarshall(), gd.getVertices(), "Matriz Warshall");
+
+        for (int i = 0; i < etiquetasarray.length; i++) {
+            System.out.println("excentricidad de " + etiquetasarray[i] + " : " + gd.obtenerExcentricidad((Comparable) etiquetasarray[i]));
+        }
+        System.out.println(gd.existeCamino("Montevideo", "Buenos_Aires"));
+        System.out.println(gd.existeCamino("Montevideo", "Porto_Alegre"));
+        String[] paises = {"Asuncion", "Buenos_Aires", "Curitiba", "Montevideo", "Porto_Alegre", "Rio_de_Janeiro", "San_Pablo", "Santos"};
+
+        for (int i = 0; i < paises.length; i++) {
+            for (int j = 0; j < paises.length; j++) {
+                if (i != j) {
+                    System.out.println("COMIENZO");
+                    gd.obtenerMejorCamino(paises[i], paises[j]);
+                    System.out.println("FIN");
+                }
+            }
+        }
+        System.out.println("Algo");
+        //System.out.println("Centro del grafo: " + gd.centroDelGrafo());
+    }
+}
