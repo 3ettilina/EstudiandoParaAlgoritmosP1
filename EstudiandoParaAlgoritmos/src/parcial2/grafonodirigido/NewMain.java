1711f4f8e4291545c7d00a2e222a4b44ada9b4b2
Juanca <Juanca@Jazz-PC>
2017-10-30 12:29:35 -0300

    buscar

--- EstudiandoParaAlgoritmos/src/parcial2/grafonodirigido/NewMain.java
+++ EstudiandoParaAlgoritmos/src/parcial2/grafonodirigido/NewMain.java
@@ -16,10 +16,9 @@ public class NewMain {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        TGrafoNoDirigido gnd = UtilGrafos.cargarGrafo("src/parcial2/implementaciones/grafonodirigido/vertices.txt", "src/parcial2/implementaciones/grafonodirigido/aristas.txt", false, TGrafoNoDirigido.class);
+        TGrafoNoDirigido gnd = UtilGrafos.cargarGrafo("src/parcial2/grafonodirigido/verticesGND.txt", "src/parcial2/grafonodirigido/aristasGND.txt", false, TGrafoNoDirigido.class);
 
         TGrafoNoDirigido aacm = gnd.Prim();
-        //System.out.println(gnd.bea("e"));
     }
 
 }
--- EstudiandoParaAlgoritmos/src/parcial2/grafonodirigido/TGrafoNoDirigido.java
+++ EstudiandoParaAlgoritmos/src/parcial2/grafonodirigido/TGrafoNoDirigido.java
@@ -1,19 +1,22 @@
 package parcial2.grafonodirigido;
+
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Map;
 
 public class TGrafoNoDirigido extends TGrafoDirigido {
-    protected TAristas lasAristas = new TAristas() ;
-       /**
+
+    protected TAristas lasAristas = new TAristas();
+
+    /**
      *
      * @param vertices
      * @param aristas
      */
     public TGrafoNoDirigido(Collection<TVertice> vertices, Collection<TArista> aristas) {
-       super(vertices, aristas);      
-      lasAristas.insertarAmbosSentidos(aristas);
+        super(vertices, aristas);
+        lasAristas.insertarAmbosSentidos(aristas);
     }
 
     @Override
@@ -23,56 +26,57 @@ public class TGrafoNoDirigido extends TGrafoDirigido {
         tempbool = (super.insertarArista(arista) && super.insertarArista(arInv));
         return tempbool;
     }
+
     public TAristas getLasAristas() {
-            return lasAristas;
-        }
+        return lasAristas;
+    }
 
     public TGrafoNoDirigido Prim() {
         int costoPrim = 0;
-        HashMap<Comparable,TVertice> U = new HashMap();
-        HashMap<Comparable,TVertice> V = new HashMap<Comparable,TVertice>();
-        TAristas AristasAAM = new TAristas();        
-        TGrafoNoDirigido nuevoGrafo = new TGrafoNoDirigido(super.copiaVertices().values(),new LinkedList<TArista>());
+        HashMap<Comparable, TVertice> U = new HashMap();
+        HashMap<Comparable, TVertice> V = new HashMap<Comparable, TVertice>();
+        TAristas AristasAAM = new TAristas();
+        TGrafoNoDirigido nuevoGrafo = new TGrafoNoDirigido(super.copiaVertices().values(), new LinkedList<TArista>());
         if (this.getVertices().isEmpty()) {
             return null;
         }
-        for(Map.Entry<Comparable,TVertice> vertice : this.getVertices().entrySet()){
+        for (Map.Entry<Comparable, TVertice> vertice : this.getVertices().entrySet()) {
             V.put(vertice.getKey(), vertice.getValue());
         }
         TVertice tempV;
         tempV = (TVertice) V.values().toArray()[0];
-	U.put(tempV.getEtiqueta(), tempV);
+        U.put(tempV.getEtiqueta(), tempV);
         V.remove(tempV.getEtiqueta());
         boolean vaciaV = V.isEmpty();
-        
+
         while (!vaciaV) {
             TArista aristaMin = this.lasAristas.buscarMin(U.values(), V.values());
             AristasAAM.add(aristaMin.copia());
             TVertice vert = V.remove(aristaMin.getEtiquetaDestino());
-            if(vert!=null){
-                U.put(vert.getEtiqueta(),vert);
+            if (vert != null) {
+                U.put(vert.getEtiqueta(), vert);
             }
-            costoPrim += aristaMin.getCosto();           
+            costoPrim += aristaMin.getCosto();
             vaciaV = V.isEmpty();
         }
-        for(TArista arista_ : AristasAAM){
+        for (TArista arista_ : AristasAAM) {
             nuevoGrafo.getLasAristas().add(arista_);
         }
         System.out.println("costo AAM: " + costoPrim);
         return nuevoGrafo;
     }
 
-      public String bea(Comparable etiquetaVertice) {
+    public String bea(Comparable etiquetaVertice) {
         TVertice vertice = buscarVertice(etiquetaVertice);
-        if (vertice != null) 
-        {
+        if (vertice != null) {
             String resultado = vertice.bea();
             this.desvisitarVertices();
             return resultado;
         }
         return "";
     }
-   private TVertice buscarVertice(Comparable unaEtiqueta) {
+
+    private TVertice buscarVertice(Comparable unaEtiqueta) {
         return getVertices().get(unaEtiqueta);
     }
 }
