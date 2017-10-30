c5057fb1c46b895dd65288b78e056a8a9ec180d3
Juanca <Juanca@Jazz-PC>
2017-10-30 12:41:00 -0300

    buscar

--- EstudiandoParaAlgoritmos/src/parcial2/grafodirigido/PruebaGrafo.java
+++ EstudiandoParaAlgoritmos/src/parcial2/grafodirigido/PruebaGrafo.java
@@ -1,18 +1,31 @@
 package parcial2.grafodirigido;
 
+import java.util.Collection;
+
 
 public class PruebaGrafo {
 
     public static void main(String[] args) {
         
-        TGrafoNoDirigido gnd = (TGrafoNoDirigido) UtilGrafos.cargarGrafo(
-                "src/UT5/TA2/vertices.txt",
-                "src/UT5/TA2/aristas.txt",
-                false, TGrafoNoDirigido.class);
-        
-        System.out.println(gnd.bea("a"));
-        
-        System.out.println(gnd.Prim().bea("f"));
+        TGrafoDirigido gd = (TGrafoDirigido) UtilGrafos.cargarGrafo("src/UT4/TA4/aeropuertos_1.txt","src/UT4/TA4/conexiones_1.txt",
+                false, TGrafoDirigido.class);
+
+        Object[] etiquetasarray = gd.getEtiquetasOrdenado();
+
+        Double[][] matriz = UtilGrafos.obtenerMatrizCostos(gd.getVertices());
+        UtilGrafos.imprimirMatrizMejorado(matriz, gd.getVertices(), "Matriz");
+        Double[][] mfloyd = gd.floyd();
+        UtilGrafos.imprimirMatrizMejorado(mfloyd, gd.getVertices(), "Matriz luego de FLOYD");
+        for (int i = 0; i < etiquetasarray.length; i++) {
+            System.out.println("excentricidad de " + etiquetasarray[i] + " : " + gd.obtenerExcentricidad((Comparable) etiquetasarray[i]));
+        }
+        System.out.println();
+        System.out.println("Centro del grafo: " + gd.centroDelGrafo());
+       
+       Collection recorrido = gd.bpf();
+       // imprimir etiquetas del bpf de todo el grafo....
+       Collection recorrido_Asuncion = gd.bpf("Santos");
+       // imprimir etiquetas del bpf desde Asunción....
         
     }
 }
