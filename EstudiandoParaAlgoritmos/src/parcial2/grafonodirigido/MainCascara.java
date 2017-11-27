9d5047025e115f0fab8d669b29f9f39bee2b41e2
Juan Carlos Díaz <jcdpess@gmail.com>
2017-11-27 08:55:21 -0300

    Merge origin/master
    Conflicts:
    	src/UT04/TA04/TGrafoDirigido.java

--- EstudiandoParaAlgoritmos/src/parcial2/grafonodirigido/MainCascara.java
+++ EstudiandoParaAlgoritmos/src/parcial2/grafonodirigido/MainCascara.java
@@ -3,57 +3,57 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package parcial2.grafonodirigido;
-
-/**
- *
- * @author Jotacé
- */
-public class MainCascara {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        int INSERCION_DIRECTA = 1;
-        int SHELLSORT = 2;
-        int BURBUJA = 3;
-        int QUICK_SORT = 4;
-
-        //Aleatorios300
-        GeneradorDatosGenericos gdg = new GeneradorDatosGenericos();
-        TClasificador clasificador = new TClasificador();
-        int[] vectorOriginal = gdg.generarDatosAscendentes(300);
-
-        Long tiempoMedioAlgoritmoBase = calculoDeTiempos(vectorOriginal, clasificador, INSERCION_DIRECTA, false);
-        Long tiempoMedioCascara = calculoDeTiempos(vectorOriginal, clasificador, INSERCION_DIRECTA, true);
-        Long tiempoMedioAlgoritmo = tiempoMedioAlgoritmoBase - tiempoMedioCascara;
-
-        System.out.println("Tiempo medio algoritmo " + tiempoMedioAlgoritmo);
-    }
-
-    private static Long calculoDeTiempos(int[] vectorOriginal, TClasificador clasificador, int numeroAlgoritmoClasificador, boolean cascara) {
-        Long t1 = System.nanoTime();
-        Long total = 0L;
-        Long tiempoResolucion = 1000000000L;
-        int cantidadLlamadas = 0;
-
-        while (total < tiempoResolucion) {
-            cantidadLlamadas += 1;
-            int[] datosCopia = copiarVector(vectorOriginal);
-            clasificador.clasificar(datosCopia, numeroAlgoritmoClasificador, cascara);
-            Long t2 = System.nanoTime();
-            total = t2 - t1;
-        }
-
-        return total / cantidadLlamadas;
-    }
-
-    public static int[] copiarVector(int[] aOrigen) {
-        int[] aDestino = new int[aOrigen.length];
-        for (int x = 0; x < aOrigen.length; x++) {
-            aDestino[x] = aOrigen[x];
-        }
-        return aDestino;
-    }
-}
+//package parcial2.grafonodirigido;
+//
+///**
+// *
+// * @author Jotacé
+// */
+//public class MainCascara {
+//
+//    /**
+//     * @param args the command line arguments
+//     */
+//    public static void main(String[] args) {
+//        int INSERCION_DIRECTA = 1;
+//        int SHELLSORT = 2;
+//        int BURBUJA = 3;
+//        int QUICK_SORT = 4;
+//
+//        //Aleatorios300
+////        GeneradorDatosGenericos gdg = new GeneradorDatosGenericos();
+////        TClasificador clasificador = new TClasificador();
+//        int[] vectorOriginal = gdg.generarDatosAscendentes(300);
+//
+//        Long tiempoMedioAlgoritmoBase = calculoDeTiempos(vectorOriginal, clasificador, INSERCION_DIRECTA, false);
+//        Long tiempoMedioCascara = calculoDeTiempos(vectorOriginal, clasificador, INSERCION_DIRECTA, true);
+//        Long tiempoMedioAlgoritmo = tiempoMedioAlgoritmoBase - tiempoMedioCascara;
+//
+//        System.out.println("Tiempo medio algoritmo " + tiempoMedioAlgoritmo);
+//    }
+//
+//    private static Long calculoDeTiempos(int[] vectorOriginal, TClasificador clasificador, int numeroAlgoritmoClasificador, boolean cascara) {
+//        Long t1 = System.nanoTime();
+//        Long total = 0L;
+//        Long tiempoResolucion = 1000000000L;
+//        int cantidadLlamadas = 0;
+//
+//        while (total < tiempoResolucion) {
+//            cantidadLlamadas += 1;
+//            int[] datosCopia = copiarVector(vectorOriginal);
+//            clasificador.clasificar(datosCopia, numeroAlgoritmoClasificador, cascara);
+//            Long t2 = System.nanoTime();
+//            total = t2 - t1;
+//        }
+//
+//        return total / cantidadLlamadas;
+//    }
+//
+//    public static int[] copiarVector(int[] aOrigen) {
+//        int[] aDestino = new int[aOrigen.length];
+//        for (int x = 0; x < aOrigen.length; x++) {
+//            aDestino[x] = aOrigen[x];
+//        }
+//        return aDestino;
+//    }
+//}
